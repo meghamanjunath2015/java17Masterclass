e196fe69c96fe839e238317a571ed608f0bd085f
meghamanjunath2015 <meghamanjunath2015@gmail.com>
2024-04-26 14:57:19 -0700

    First challenge add

---
+++ src/src/opChallenge.java
@@ -0,0 +1,11 @@
+public class opChallenge {
+    public static void main(String[] args) {
+        double a = 20.00d;
+        double b = 80.00d;
+        double c = (a + b) * 100d;
+        double reminder = c % 40.00d;
+        System.out.println("reminder:" + reminder);
+        boolean isReminderZero = (reminder == 0) ? true : false;
+        System.out.println(isReminderZero);
+    }
+}
